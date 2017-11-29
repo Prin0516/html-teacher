09fc9ac4470892e41889edc3c864b78c3a9f30a7
Prin0516 <105021046@live.asia.edu.tw>
2017-11-29 11:52:14 +0800

    1

--- mysql_connect.inc.php
+++ mysql_connect.inc.php
@@ -1,21 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Connect-Type" content="text/html";charset="utf-8"/>
-</head>
-<body>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
-$db_server="120.108.117.245/phpmyadmin";
-$db_name="105021046";
-$db_user="105021046";
-$db_passwd="#3bAcCsEa";
-if(!@mysql_connect($db_server,$db_user,$db_passwd))
-    die("無法對資料庫連線");
+//資料庫設定
+//資料庫位置
+//$db_server = "localhost";
+////資料庫名稱
+//$db_name = "105021046";
+////資料庫管理者帳號
+//$db_user = "105021046";
+////資料庫管理者密碼
+//$db_passwd = "#3bAcCsEa";
+//對資料庫連線
+$link=mysqli_connect("localhost","105021046", "#3bAcCsEa","105021046");
+if($link){
+    echo"123";
+}
 
-mysql_query("SET NAMES utf-8");
+////資料庫連線採UTF8
+//mysql_query("SET NAMES utf8");
+//
+////選擇資料庫
+//if(!@mysql_select_db($db_name))
+//    die("無法使用資料庫");
 
-if(!@mysql_select_db($db_name))
-    die("無法使用資料庫");
 ?>
-</body>
-</html>
