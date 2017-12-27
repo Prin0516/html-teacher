123a91b7fe95ec588499ce20b729a0029d2ba80c
Prin0516 <105021046@live.asia.edu.tw>
2017-12-27 10:05:58 +0800

    222

---
+++ sendmail.php
@@ -0,0 +1,40 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'mail/vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug =0;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'username@gmail.com';
+    $mail->Password = '_password_';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+
+    $mail->setFrom('sender@example.com', 'Admin');
+    $mail->addAddress('recipient1@example.net', 'Recipient1');
+    $mail->addAddress('recipient2@example.com');
+//    $mail->addReplyTo('noreply@example.com', 'noreply');
+//    $mail->addCC('cc@example.com');
+//    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/backup/myfile.tar.gz');
+
+    //Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Test Mail Subject!';
+    $mail->Body    = 'This is SMTP Email Test';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
